f37fb643621644c3fbc68e4fd312a468d3b5c52d
Unknown <tsiclovan947@west-mec.org>
2018-10-16 18:21:58 -0700

    Project Continuation
    Created different field names to submit to a text file; Began to add PHP to the gamer form; Brainstormed how info will be displayed beneath the form

--- TheGame.php
+++ TheGame.php
@@ -14,15 +14,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1.0">
     <script src="modernizr.custom.65897.js"></script>
+    <link rel="stylesheet" href="gamers.css">
 </head>
 
 <body>
-    <h2 style="text-align: center">The Game</h2>
     <?php
-    
+    $dir = "./comments";
+    if (is_dir($dir)) {
+        if (isset($_POST['save'])) {
+            if (empty($_POST['name'])) {
+                echo "Unknown visitor\n";
+            }
+            else {
+                $saveString = stripslashes($_POST['username']) . "\n";
+                $saveString = stripslashes($_POST['pswd']) . "\n";
+                $saveString = stripslashes($_POST['fname']) . "\n";
+                $saveString = stripslashes($_POST['lname']) . "\n";
+                $saveString .= stripslashes($_POST['email']) . "\n";
+                $saveString .= stripslashes($_POST['age']) . "\n";
+                $saveString .= stripslashes($_POST['sname']) . "\n";
+                $saveString .= stripslashes($_POST['comment']) . "\n";
+                $saveString .= date('r') . "\n";
+                $saveString .= stripslashes($_POST['comment']) . "\n";
+                echo "\$saveString: $saveString<br>";
+                $currentTime = microtime();
+                echo "\$currentTime: $currentTime<br>";
+                $timeArray = explode(" ", $currentTime);
+                echo var_dump($timeArray) . "<br>";
+                $timeStamp = (float)$timeArray[1] + (float)$timeArray[0];
+                echo "\$timeStamp: $timeStamp<br>";
+                $saveFileName = "$dir/Comment.$timeStamp.txt";
+                echo "\$saveFileName: $saveFileName<br>";
+                if (file_put_contents($saveFileName, $saveString) > 0) {
+                    echo "File \"" . htmlentities($saveFileName) . "\"successfully saved.<br>\n";
+                }
+                else {
+                    echo "There was an error writing \"" . htmlentities($saveFileName) . "\".<br>\n";
+                }
+            }
+        }
+    }
+    else {
+        mkdir($dir);
+        chmod($dir, 0666);
+    }
     ?>
 </body>
+<br>
+<h2 style="text-align: center">The Game</h2>
  <form action="TheGame.php" method="post" style="text-align: center">
+       <br>
+       <br>
         Username: <input type="text" name="username"><br><br>
         Password: <input type="password" name="pswd" maxlength="10"><br><br>
         First Name: <input type="text" name="fname"><br><br>
